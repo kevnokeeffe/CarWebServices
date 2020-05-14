80efeadd17db19040b272a8b0b4a01237c215674
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-05-14 21:05:16 +0100

    Updated with comments

--- build/generated-sources/ap-source-output/carWebService/Cars_.java
+++ build/generated-sources/ap-source-output/carWebService/Cars_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-09T20:53:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-12T16:19:58")
 @StaticMetamodel(Cars.class)
 public class Cars_ { 
 
